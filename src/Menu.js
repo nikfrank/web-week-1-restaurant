001a899738719d0649b9b373092fe8548f7be716
nik frank <nikbfrank@gmail.com>
2019-07-11 02:31:06 -0700

    menu i18n

--- src/Menu.js
+++ src/Menu.js
@@ -1,5 +1,50 @@
 import React from 'react';
+import menu from './menu-database';
+import './Menu.css';
 
-const Menu = ()=> (<div>Menu</div>);
+class Menu extends React.Component {
+  state = {
+    currentLanguage: 'en',
+    languages: ['en', 'fr', 'he'],
+  }
+
+  setLanguage = (event)=>{
+    this.setState({
+      currentLanguage: event.target.value,
+    })
+  }
+
+  render(){
+    const currentLanguage = this.state.currentLanguage;
+
+    return (
+      <div className='Menu Page'>
+        <select value={currentLanguage}
+                onChange={this.setLanguage}>
+          <option value='en'>English</option>
+          <option value='fr'>French</option>
+          <option value='he'>Hebrew</option>
+        </select>
+
+        <div className='menu-container'>
+          {menu.map(menuPage=> (
+            <div className='menu-page' key={menuPage.title[currentLanguage]}>
+              <h3>{menuPage.title[currentLanguage]}</h3>
+
+              {menuPage.menuItems.map(menuItem=> (
+                <div className={currentLanguage === 'he' ? 'menu-item hebrew' : 'menu-item'}
+                     key={menuItem.name[currentLanguage]}>
+                  <span>{menuItem.name[currentLanguage]}</span>
+                  <span className='dotted-connector'><span/></span>
+                  <span>${menuItem.price}</span>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Menu;
---
+++ src/menu-database.js
@@ -0,0 +1,127 @@
+export default [
+  {
+    title: {
+      en: 'Classics',
+      fr: 'Classiques',
+      he: 'קלסיקה',
+    },
+    menuItems: [
+      {
+        name: {
+          en: 'fish tacos',
+          fr: 'tacos au poisson',
+          he: 'טקות דג',
+        },
+        price: 4,
+      },
+      {
+        name: {
+          en: 'beef taco',
+          fr: 'taco au boeuf',
+          he: 'טקות בשר',
+        },
+        price: 5,
+      },
+      {
+        name: {
+          en: 'treif taco',
+          fr: 'tacos au porc',
+          he: 'טקות חזיר',
+        },
+        price: 8,
+      },
+      {
+        price: 3.5,
+        name: {
+          en: 'ice cream taco',
+          fr: 'taco glace',
+          he: 'טקות גלידה',
+        },
+      },
+    ],
+  },
+
+  {
+    title: {
+      en: 'Supreme tacos',
+      fr: 'Supremes',
+      he: 'סופרימה',
+    },
+    menuItems: [
+      {
+        name: {
+          en: 'supreme fish tacos',
+          fr: 'tacos au poisson supreme',
+          he: 'טקות דג',
+        },
+        price: 4,
+      },
+      {
+        name: {
+          en: 'supreme beef taco',
+          fr: 'taco au boeuf supreme',
+          he: 'טקות בשר',
+        },
+        price: 5,
+      },
+      {
+        name: {
+          en: 'supreme treif taco',
+          fr: 'tacos au porc supreme',
+          he: 'טקות חזיר',
+        },
+        price: 8,
+      },
+      {
+        price: 3.5,
+        name: {
+          en: 'supreme ice cream taco',
+          fr: 'taco glace supreme',
+          he: 'טקות גלידה',
+        },
+      },
+    ],
+  },
+
+  {
+    title: {
+      en: 'Bonus',
+      fr: 'Bonuit',
+      he: 'בונוס',
+    },
+    menuItems: [
+      {
+        name: {
+          en: 'bonus fish tacos',
+          fr: 'tacos au poisson bonuit',
+          he: 'טקות דג',
+        },
+        price: 4,
+      },
+      {
+        name: {
+          en: 'bonus beef taco',
+          fr: 'taco au boeuf bonuit',
+          he: 'טקות בשר',
+        },
+        price: 5,
+      },
+      {
+        name: {
+          en: 'bonus treif taco',
+          fr: 'tacos au porc bonuit',
+          he: 'טקות חזיר',
+        },
+        price: 8,
+      },
+      {
+        price: 3.5,
+        name: {
+          en: 'bonus ice cream taco',
+          fr: 'taco glace bonuit',
+          he: 'טקות גלידה',
+        },
+      },
+    ],
+  },
+];
